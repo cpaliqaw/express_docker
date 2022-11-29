b3e7853d127ac4ba8e351b4eee9c528f2eb6272f
Chris Paliqaw <cpaliqaw@robotnik.es>
2022-11-29 13:15:26 +0100

    feature: add cors

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## About
 
 Used tutorial [Dockerizing a Node.js web app](https://nodejs.org/en/docs/guides/nodejs-docker-webapp/)
+
+## Use
+
+docker build . -t hmi:0.1
+
+docker run -p 49160:8080 -d hmi:0.1
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 // Constants
 const PORT = 8080;
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-// app.use(cors);
+app.use(cors);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
